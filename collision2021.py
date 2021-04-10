69cd48a02d615d7d0e261ebd2f91e5bd1e23d974
seong-yeop <ucassje@ucl.ac.uk>
2021-04-10 13:33:00 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -22,7 +22,7 @@ Nv=30  #velocity step number
 i_solar_r=5 #10
 f_solar_r=20 #30
 path_home="/Users/user/Desktop/JSY_Code/"
-path_lab="/disk/plasma4/syj2/Code/JSY_Code/"
+path_lab="/disk/plasma4/syj2/Code/JSY_Code/JSY_Code/"
 # path_current=path_home
 path_current=path_lab
 def n_0(r):
