45a7295e0effe1dfeb9291cfd6c45de1bcf1c451
seong-yeop <ucassje@ucl.ac.uk>
2021-04-13 11:20:23 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -814,12 +814,19 @@ for k in range(timestep):
 X2,Y2 = np.meshgrid(pal_v,per_v)
 cont_lev = np.linspace(-10,0,25)
 
-p = lmfit.Parameters()
-p.add_many(('nc', 0.9,True,0.7,1), ('Tc_pal', 5*10**5,True,1*10**5,10*10**5), ('Ts_pal', 8*10**5,True,1*10**5,10*10**5), ('Ts_per', 8*10**5,True,1*10**5,10*10**5), ('Uc',-0.1,True,-0.4,0),('Us',0.6,True,0.5,0.7),('kappac',8,True,4,20),('kappas',3,True,2,10))
+#p = lmfit.Parameters()
+#p.add_many(('nc', 0.9,True,0.7,1), ('Tc_pal', 5*10**5,True,1*10**5,10*10**5), ('Ts_pal', 8*10**5,True,1*10**5,10*10**5), ('Ts_per', 8*10**5,True,1*10**5,10*10**5), ('Uc',-0.1,True,-0.4,0),('Us',0.6,True,0.5,0.7),('kappac',8,True,4,20),('kappas',3,True,2,10))
 #('ns', 0.01,True,0,0.05)
  
 for r in range(Nr):
     print(r)
+    if r==0:
+            p = lmfit.Parameters()
+            p.add_many(('nc', 1,True,0.7,1), ('Tc_pal', 10*10**5,True,1*10**5,10*10**5), ('Ts_pal', 0*10**5,True,1*10**5,10*10**5), ('Ts_per', 0*10**5,True,1*10**5,10*10**5), ('Uc',0,True,-0.4,0),('Us',0,True,0,0.7),('kappac',8,True,4,20),('kappas',3,True,2,10))
+    else:
+            p = lmfit.Parameters()
+            p.add_many(('nc', nc,True,0.7,1), ('Tc_pal', Tc_pal,True,1*10**5,10*10**5), ('Ts_pal', Ts_pal,True,1*10**5,10*10**5), ('Ts_per', Ts_per,True,1*10**5,10*10**5), ('Uc',Uc,True,-0.4,0.4),('Us',Us,True,0.,0.7),('kappac',kappac,True,4,20),('kappas',kappas,True,2,10))
+
     f_11=np.zeros(shape = (Nv**2, 1))
     for j in range(Nv):
             for i in range(Nv):
