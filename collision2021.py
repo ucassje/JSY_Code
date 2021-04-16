de59aeca25d959c3ccf2fb3e6cb9d68e59a71c60
seong-yeop <ucassje@ucl.ac.uk>
2021-04-16 20:06:42 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -578,7 +578,7 @@ f_temp=np.zeros(shape = (Nr*Nv**2, 1))
 f_temp[:,:]=f_1[:,:]
 kl=50
 
-timestep=1000 #700
+timestep=3000 #700
 Normvalue=np.zeros(shape = (timestep))
 for k in range(timestep):
     print(k)
