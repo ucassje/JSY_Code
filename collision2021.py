117c9a324bb879c23977290c9fa715de44e6d874
seong-yeop <ucassje@ucl.ac.uk>
2021-04-16 20:06:13 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -87,6 +87,9 @@ def dU_solar(x):
 def cos(r):
         return (1/(1+(r*Omega/U_solar(r))**2)**0.5)
 
+def dcos_1(r):
+        return ((r*(Omega/U_solar(r))**2-(r*Omega/U_solar(r))**2/U_solar(r)*dU_solar(x))/(1+(r*Omega/U_solar(r))**2)**0.5)
+
 def temperature(r):
         return T_e*(i_solar_r/r)**(0.8) #T_e*np.exp(-(r-i_solar_r)**2/600) #T_e*np.exp(2/(r-2.2)**0.7) #(0.1*T_e-T_e)/(f_solar_r-i_solar_r)*(r-i_solar_r)+T_e
 
@@ -325,7 +328,7 @@ def sin(x):
         return (1/(1+(U_solar(x)/Omega*(1/x))**2)**0.5)
 
 def dcos(x):
-        return -((Omega/U_solar(x))**2*x)/(1+(x*Omega/U_solar(x))**2)**1.5
+        return -1/(1+(x*Omega/U_solar(x))**2)**1.5*(((Omega/U_solar(x))**2*x)-x**2*(Omega**2/U_solar(x)**3)*dU_solar(x))
 
 def dsin(x):
         return ((U_solar(x)/Omega*(1/x))**2*(1/x**3))/(1+(U_solar(x)/Omega*(1/x))**2)**1.5
@@ -340,7 +343,7 @@ def dlnB(x):
         return (np.log(B(x+delz))-np.log(B(x-delz)))/(2*delz)
 
 def electric(x):
-        return U_solar(x)*dU_solar(x)+(1/v_Ae_0**2)*(Bol_k)/(Me*n(x))*(n(x)*temperature(x)*lntemperature(x)+temperature(x)*n(x)*lnn(x))+(1/v_Ae_0**2)*(Bol_k)/(2*Me)*dlnB(x)*temperature(x)+(1/v_Ae_0**2)*(2*Bol_k)/(Me*x)*temperature(x)
+        return U_solar(x)*dU_solar(x)/(cos(x)**2)+(U_solar(x)**2/cos(x))*dcos_1(x)+(1/v_Ae_0**2)*(Bol_k)/(Me*n(x))*(n(x)*temperature(x)*lntemperature(x)+temperature(x)*n(x)*lnn(x))+(1/v_Ae_0**2)*(Bol_k)/(2*Me)*dlnB(x)*temperature(x)+(1/v_Ae_0**2)*(2*Bol_k)/(Me*x)*temperature(x)
 
 def Matrix_A(R,M):
     A=np.zeros(((Nv),(Nv)))
