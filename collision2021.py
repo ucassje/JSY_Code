a2626feb9dbd44fbf03bd62b2c8cbeb0e82710e0
seong-yeop <ucassje@ucl.ac.uk>
2021-04-19 11:40:26 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -733,8 +733,8 @@ for k in range(timestep):
     for R in range(Nr):
             for J in range(Nv):
                     for I in range(Nv):
-                            #if R==Nr-1:#and pal_v[I]>0 and (pal_v[I]**2+per_v[J]**2)**0.5<5
-                            norm=norm+abs((f_next[R*(Nv)*(Nv)+J*Nv+I]/np.max(f_next)-f_pre[R*(Nv)*(Nv)+J*Nv+I]/np.max(f_next)))**2
+                            if np.log10(f_next[R*(Nv)*(Nv)+J*Nv+I]/np.max(f_next))>-5:
+                                    norm=norm+abs((f_next[R*(Nv)*(Nv)+J*Nv+I]/np.max(f_next)-f_pre[R*(Nv)*(Nv)+J*Nv+I]/np.max(f_next)))**2
     Normvalue[k]=norm**0.5
     print(norm**0.5)
 
@@ -784,7 +784,7 @@ plt.rc('font', size=35)
 plt.tick_params(labelsize=40)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 ax.set_xlim([o[0],o[timestep-1]])
-ax.set_ylim([10**(-6),10**(-4)])
+ax.set_ylim([10**(-6),10**(-3)])
 ax.set_xlabel(r'$t$', fontsize=28)
 ax.set_ylabel(r'$norm$', fontsize=28)
 ax.plot(o,Normvalue,linewidth=3.0, color='k');
