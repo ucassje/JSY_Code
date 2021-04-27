e99e53c2f803f9983a0c05a94f69d5ae93eed867
seong-yeop <ucassje@ucl.ac.uk>
2021-04-27 19:42:29 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -586,7 +586,7 @@ kl=50
 
 np.save('data_pre.npy', f_1)
 
-timestep=10 #700
+timestep=3000 #700
 Normvalue=np.zeros(shape = (timestep))
 Normvalue_bulk=np.zeros(shape = (timestep))
 for k in range(timestep):
@@ -644,41 +644,40 @@ for k in range(timestep):
     
     f_1=dot(AQ, f_1)       
 
-    #Density_next=np.zeros(shape = (Nr))
-    #for r in range(Nr):
-    #    tempDensity=0
-    #    for j in range(Nv):
-    #        for i in range(Nv):
-    #            if per_v[j]<0:
-    #                  tempDensity=tempDensity
-    #            else:
-    #                  tempDensity=tempDensity+2*np.pi*f_1[r*(Nv)*(Nv)+j*Nv+i]*abs(per_v[j])*(pal_v[1]-pal_v[0])**2
-    #    Density_next[r]=tempDensity/(r_s**3)
+    Density_next=np.zeros(shape = (Nr))
+    for r in range(Nr):
+        tempDensity=0
+        for j in range(Nv):
+            for i in range(Nv):
+                if per_v[j]<0:
+                      tempDensity=tempDensity
+                else:
+                      tempDensity=tempDensity+2*np.pi*f_1[r*(Nv)*(Nv)+j*Nv+i]*abs(per_v[j])*(pal_v[1]-pal_v[0])**2
+        Density_next[r]=tempDensity/(r_s**3)
 
 
 
-    #Bulk_next=np.zeros(shape = (Nr))
-    #for r in range(Nr):
-    #    tempBulk=0
-    #    for j in range(Nv):
-    #        for i in range(Nv):
-    #            if per_v[j]>=0:
-    #                  tempBulk=tempBulk+2*np.pi*pal_v[i]*f_1[r*(Nv)*(Nv)+j*Nv+i]*abs(per_v[j])*(pal_v[1]-pal_v[0])**2
-    #            else:
-    #                  tempBulk=tempBulk
-    #    Bulk_next[r]=tempBulk/((r_s**3)*Density_next[r])
+    Bulk_next=np.zeros(shape = (Nr))
+    for r in range(Nr):
+        tempBulk=0
+        for j in range(Nv):
+            for i in range(Nv):
+                if per_v[j]>=0:
+                      tempBulk=tempBulk+2*np.pi*pal_v[i]*f_1[r*(Nv)*(Nv)+j*Nv+i]*abs(per_v[j])*(pal_v[1]-pal_v[0])**2
+                else:
+                      tempBulk=tempBulk
+        Bulk_next[r]=tempBulk/((r_s**3)*Density_next[r])
 
     
-    #f_temp6=np.zeros(shape = (Nr*Nv**2, 1))
-    #f_temp6[:,:]=f_1[:,:]
-    #for r in range(Nr):
-    #    for j in range(Nv):
-    #        for i in range(Nv):
-    #                if r==0 or r==Nr-1 or i==0 or i==Nv-1:
-    #                        f_1[r*(Nv)*(Nv)+j*Nv+i]=f_temp6[r*(Nv)*(Nv)+j*Nv+i]
-    #                else:
-    #                        f_1[r*(Nv)*(Nv)+j*Nv+i]=f_temp6[r*(Nv)*(Nv)+j*Nv+i]-(U_solar(z[r])*dU_solar(z[r])/cos(z[r])+U_solar(z[r])**2*dcos_1(z[r])+(cos(z[r])/v_Ae_0**2)*(Bol_k)/(Me*Density_pre[r])*(Density_pre[r+1]*Temperature_pal[r+1]-Density_pre[r-1]*Temperature_pal[r-1])/(2*delz)-(cos(z[r])/v_Ae_0**2)*(Bol_k)/(Me)*dlnB(z[r])*Temperature_pal[r]+(cos(z[r])/v_Ae_0**2)*(Bol_k)/(2*Me)*dlnB(z[r])*Temperature_per[r]+(cos(z[r])/v_Ae_0**2)*(2*Bol_k)/(Me*z[r])*Temperature_pal[r]+(2/(Density_next[r]+Density_pre[r]))*(Density_next[r]*Bulk_next[r]-Density_pre[r]*Bulk_pre[r])/delt)*(delt/(2*delv))*(f_temp6[r*(Nv)*(Nv)+j*Nv+i+1]-f_temp6[r*(Nv)*(Nv)+j*Nv+i-1]) #f_temp6[r*(Nv)*(Nv)+j*Nv+i]-(2/(Density_next[r]+Density_pre[r]))*(Density_next[r]*Bulk_next[r]-Density_pre[r]*Bulk_pre[r])*(1/(2*delv))*(f_temp6[r*(Nv)*(Nv)+j*Nv+i+1]-f_temp6[r*(Nv)*(Nv)+j*Nv+i-1])
-
+    f_temp6=np.zeros(shape = (Nr*Nv**2, 1))
+    f_temp6[:,:]=f_1[:,:]
+    for r in range(Nr):
+        for j in range(Nv):
+            for i in range(Nv):
+                    if abs(pal_v[i])<0.5:
+                            f_1[r*(Nv)*(Nv)+j*Nv+i]=f_temp6[r*(Nv)*(Nv)+j*Nv+i]
+                    else:
+                            f_1[r*(Nv)*(Nv)+j*Nv+i]=(pal_v[i]-Bulk_next[r])/pal_v[i]*f_temp6[r*(Nv)*(Nv)+j*Nv+i]
 
 
     f_temp4=np.zeros(shape = (Nr*Nv**2, 1))
@@ -960,10 +959,7 @@ np.save('data_next.npy', f_1)
 X2,Y2 = np.meshgrid(pal_v,per_v)
 cont_lev = np.linspace(-10,0,25)
 
-#p = lmfit.Parameters()
-#p.add_many(('nc', 0.9,True,0.7,1), ('Tc_pal', 5*10**5,True,1*10**5,10*10**5), ('Ts_pal', 8*10**5,True,1*10**5,10*10**5), ('Ts_per', 8*10**5,True,1*10**5,10*10**5), ('Uc',-0.1,True,-0.4,0),('Us',0.6,True,0.5,0.7),('kappac',8,True,4,20),('kappas',3,True,2,10))
-#('ns', 0.01,True,0,0.05)
- 
+
 
 
 number=0
