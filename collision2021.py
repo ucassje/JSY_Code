831018e0420ff34808e7ff96da04bb34903d9339
seong-yeop <ucassje@ucl.ac.uk>
2021-04-28 23:42:01 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -26,12 +26,13 @@ path_lab="/disk/plasma4/syj2/Code/JSY_Code/JSY_Code/"
 # path_current=path_home
 path_current=path_lab
 def n_0(r):
-        return 1*(215/r)**2
+        return 10*(215/r)**2
 
 def B_0(r):
         return 10*(215/r)**2
 
 v_Ae_0=(B_0(215)*10**(-9))/(4.*np.pi*10**(-7)*9.1094*10**(-31)*n_0(215)*10**6)**0.5
+print(v_Ae_0)
 q=1.6022*(10**(-19))
 Me=9.1094*(10**(-31))
 Mp=1.6726*(10**(-27))
