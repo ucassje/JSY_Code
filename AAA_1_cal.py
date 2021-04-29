66b245c43b18678c4e5d5e6f9270967a14047aad
seong-yeop <ucassje@ucl.ac.uk>
2021-04-29 19:10:10 +0100

    update

---
+++ AAA_1_cal.py
@@ -0,0 +1,25 @@
+from numpy.linalg import inv
+from numpy import dot
+from numpy import pi,exp,sqrt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from mpmath import *
+from matplotlib.ticker import MultipleLocator
+import matplotlib as mpl
+from math import gamma
+import math as math
+import numpy as np
+from scipy import integrate
+from scipy import special
+from math import e
+from tempfile import TemporaryFile
+from numpy import exp, loadtxt, pi, sqrt
+from lmfit import Parameters, fit_report, minimize
+#from lmfit import Model
+import lmfit
+
+AAA = np.load('AAA.npy')
+
+AAA_1 = inv(AAA)
+
+np.save('AAA_1.npy', AAA_1)
