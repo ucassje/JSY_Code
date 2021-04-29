4f7a92bf6ae9aa2c066fff17c68161a07eb5f545
seong-yeop <ucassje@ucl.ac.uk>
2021-04-29 15:38:02 +0100

    update

--- collision2021.py
+++ collision2021.py
@@ -43,7 +43,7 @@ pal_v = np.linspace(-Mv, Mv, Nv)
 per_v = np.linspace(-Mv, Mv, Nv)
 delv=pal_v[1]-pal_v[0]
 print(delv)
-Nr=30      #radial step number
+Nr=50      #radial step number
 r_s=696340000.
 z=np.linspace(i_solar_r, f_solar_r, Nr)
 delz=z[1]-z[0]
